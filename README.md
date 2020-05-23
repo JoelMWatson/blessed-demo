994bf61aab474ffde12b5d6d7fd95dde4f3336d2
Joel Watson <Joel.watson92@gmail.com>
2020-05-23 11:56:10 -0700

    Finished demoing blessed

---
+++ README.md
@@ -0,0 +1,15 @@
+# Blessed Demo
+
+This is a simple login form (Design only non-functioning) for the terminal. I did
+this demo to test out blessed as an option for a project but decided to go with
+a simpler package due to time constraints. That being said this is a great package.
+
+## Author: Joel Watson
+
+### Setup
+
+- `npm install`
+
+### How to run
+
+- `npm start`
---
+++ app.js
@@ -0,0 +1,143 @@
+const blessed = require('blessed');
+
+// Screen
+const screen = blessed.screen({
+  smartCSR: true,
+  title: 'Blessed form',
+});
+
+// Form
+const form = blessed.form({
+  parent: screen,
+  width: '90%',
+  left: 'center',
+  keys: true,
+  vi: true,
+});
+
+// Name
+const usernameLable = blessed.text({
+  parent: screen,
+  top: 3,
+  left: 5,
+  content: 'FIRST NAME:',
+});
+const username = blessed.textbox({
+  parent: form,
+  name: 'username',
+  top: 2,
+  left: 5,
+  height: 3,
+  inputOnFocus: true,
+  content: 'username',
+  border: {
+    type: 'line',
+  },
+  focus: {
+    fg: 'blue',
+  },
+});
+
+const passwordLabel = blessed.text({
+  parent: screen,
+  content: 'PASSWORD:',
+  top: 7,
+  left: 5,
+});
+const password = blessed.textbox({
+  parent: form,
+  name: 'password',
+  top: 6,
+  left: 5,
+  height: 3,
+  inputOnFocus: true,
+  content: '********',
+  border: {
+    type: 'line',
+  },
+  focus: {
+    fg: 'blue',
+  },
+});
+
+const submit = blessed.button({
+  parent: form,
+  name: 'submit',
+  content: 'Submit',
+  top: 10,
+  left: 5,
+  shrink: true,
+  padding: {
+    top: 1,
+    right: 2,
+    bottom: 1,
+    left: 2,
+  },
+  style: {
+    bold: true,
+    fg: 'white',
+    bg: 'green',
+    focus: {
+      inverse: true,
+    },
+  },
+});
+
+const cancel = blessed.button({
+  parent: form,
+  name: 'cancel',
+  content: 'Cancel',
+  top: 10,
+  left: 15,
+  shrink: true,
+  padding: {
+    top: 1,
+    right: 2,
+    bottom: 1,
+    left: 2,
+  },
+  style: {
+    bold: true,
+    fg: 'white',
+    bg: 'red',
+    focus: {
+      inverse: true,
+    },
+  },
+});
+
+// Info
+var msg = blessed.message({
+  parent: screen,
+  top: 14,
+  left: 5,
+  style: {
+    italic: true,
+    fg: 'green',
+  },
+});
+
+// Event management
+submit.on('press', function () {
+  form.submit();
+});
+cancel.on('press', function () {
+  screen.destroy();
+});
+
+form.on('submit', function (data) {
+  msg.setContent(
+    'Form Submitted: { Username: ' +
+      data.username +
+      ', Password: ' +
+      data.password +
+      ' }',
+  );
+});
+
+// Key bindings
+screen.key('q', function () {
+  this.destroy();
+});
+
+module.exports = screen;
